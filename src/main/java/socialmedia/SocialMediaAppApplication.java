fbbbb6452391ff562bb7e0afa25f654d709ba2c0
mdchvatal <mdchvatal@gmail.com>
2021-07-30 15:29:02 -0400

    Adds CORS configuration for testing

--- src/main/java/socialmedia/SocialMediaAppApplication.java
+++ src/main/java/socialmedia/SocialMediaAppApplication.java
@@ -2,12 +2,28 @@ package socialmedia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SocialMediaAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SocialMediaAppApplication.class, args);
+	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "OPTIONS", "PUT")
+                .allowedHeaders("Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
+                        "Access-Control-Request-Headers")
+                .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials");
+			}
+		};
 	}
 
 }
--- src/main/java/socialmedia/controllers/UserController.java
+++ src/main/java/socialmedia/controllers/UserController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,7 @@ public class UserController {
 	@Autowired
 	private UserServiceImpl userService;
 	
+	@CrossOrigin(origins = "http://localhost:8080")
 	@GetMapping("/users/{userName}/timeline")
 	public List<Message> getUserStory(@PathVariable String userName) {
 		return userService.getUserTimelineByUserName(userName);
